f17301f425cec46d9a14895c57291871fed9c5e0
tarikguven <tarikguven74@gmail.com>
2023-04-30 11:09:06 +0300

    ....

--- hellomod/parser.go
+++ hellomod/parser.go
@@ -1,6 +1,6 @@
 package hellomod
 
-func GetFile(text string, text2 string, text3 string) string {
+func GetFile(text string, text2 string, text3 string, text4 string) string {
 
 	return text + " " + text2
 }
